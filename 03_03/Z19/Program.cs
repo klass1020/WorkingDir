0dde0d115033c20234e44f5b0bbf841b49dcac85
EfremovaLolita <klass1020@yandex.ru>
2023-03-03 11:30:02 +0300

    Zadacha19

---
+++ 03_03/Z19/Program.cs
@@ -0,0 +1,15 @@
+﻿string a;
+a = Console.ReadLine();
+char Pervoe = a[0];
+char Vtoroe = a[1];
+char Cetvertoe = a[3];
+char Pyatoe = a[4];
+if( Pervoe == Pyatoe && Vtoroe == Cetvertoe )
+{
+Console.WriteLine("Palindrom");
+
+}
+else
+{
+    Console.WriteLine("Ne Palindrom");
+}
