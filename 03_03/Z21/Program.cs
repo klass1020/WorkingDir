bd673c4ea74b06a66244d79ce70947bae503f74b
EfremovaLolita <klass1020@yandex.ru>
2023-03-03 12:45:24 +0300

    19, 21, 23

---
+++ 03_03/Z21/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Bведите x1");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Bведите y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите z1");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Bведите x2");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Bведите y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите z2");
+int z2 = int.Parse(Console.ReadLine());
+double Rasstoyanie = Math.Sqrt(Math.Pow( x2 - x1, 2)+ Math.Pow( y2 - y1, 2)+ Math.Pow ( z2 - z1, 2));
+Console.WriteLine(Rasstoyanie);
---
+++ 03_03/Z23/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = int.Parse(Console.ReadLine());
+for(int i = 1; i <= a; i = i + 1 )
+{
+    Console.WriteLine(Math.Pow(i, 3));
+          
+}
